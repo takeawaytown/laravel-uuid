cd4993a943b91a653af96860e1ef3a863f2cee3f
Alex Scott <codetheorist@gmail.com>
2017-11-14 22:11:42 +0000

    Code style update

--- src/Classes/Uuid.php
+++ src/Classes/Uuid.php
@@ -97,187 +97,189 @@ class Uuid
         bin2hex(substr($uuid, 10, 6));
     }
 
-  /**
-   * Generates a UUID, with the verion being based on $ver
-   *
-   * @param  integer $ver  The UUID version to use. Currently versions 1,3,4 and 5 are supported
-   * @param  string  $node Name string required for version 3 or 5 UUIDs
-   * @param  string  $ns   Binary namespace is required for version 3 or 5 UUIDs
-   * @return UUID          Returns a formatted UUID
-   * @throws Exception
-   */
-  public static function generate($ver = null, $node = null, $ns = null) {
+    /**
+    * Generates a UUID, with the verion being based on $ver
+    *
+    * @param  integer $ver  The UUID version to use. Currently versions 1,3,4 and 5 are supported
+    * @param  string  $node Name string required for version 3 or 5 UUIDs
+    * @param  string  $ns   Binary namespace is required for version 3 or 5 UUIDs
+    * @return UUID          Returns a formatted UUID
+    * @throws Exception
+    */
+    public static function generate($ver = null, $node = null, $ns = null) {
 
-    $ver = $ver !== null ? $ver : config('uuid.default_version');
-    $node = $node !== null ? $node : config('uuid.default_node');
-    /* Create a new UUID based on provided data. */
-    switch ((int)$ver) {
-      case 1:
-        return new static(static::timeGenerator($node));
-      case 2:
-        throw new Exception('Version ' . $ver . ' is unsupported.');
-      case 3:
-        return new static(static::nameGenerator(static::MD5, $node, $ns));
-      case 4:
-        return new static(static::randomGenerator());
-      case 5:
-        return new static(static::nameGenerator(static::SHA1, $node, $ns));
-      default:
-        throw new Exception('Version ' . $ver . ' is unsupported.');
+        $ver = $ver !== null ? $ver : config('uuid.default_version');
+        $node = $node !== null ? $node : config('uuid.default_node');
+        /* Create a new UUID based on provided data. */
+        switch ((int)$ver) {
+            case 1:
+                return new static(static::timeGenerator($node));
+            case 2:
+                throw new Exception('Version ' . $ver . ' is unsupported.');
+            case 3:
+                return new static(static::nameGenerator(static::MD5, $node, $ns));
+            case 4:
+                return new static(static::randomGenerator());
+            case 5:
+                return new static(static::nameGenerator(static::SHA1, $node, $ns));
+            default:
+                throw new Exception('Version ' . $ver . ' is unsupported.');
+        }
     }
-  }
-
-  /**
-   * Generates a UUID.
-   *
-   * @param string $node Name string required for version 3 or 5 UUIDs
-   * @return string
-   */
-  protected static function timeGenerator($node = null)
-  {
-
-    $time = static::timeString();
-
-    // Reorder bytes to their proper locations in the UUID
-    $uuid = $time[4] . $time[5] . $time[6] . $time[7] . $time[2] . $time[3] . $time[0] . $time[1];
-
-    // Generate a random clock sequence
-    $uuid .= random_bytes(2);
-
-    // set variant
-    $uuid[8] = chr(ord($uuid[8]) & static::CLEAR_VAR | static::VAR_RFC);
-
-    // set version
-    $uuid[6] = chr(ord($uuid[6]) & static::CLEAR_VER | static::VERSION_1);
-
-    // Set the final 'node' parameter, a MAC address
-    if (!is_null($node)) {
-        $node = static::makeBinary($node, 6);
-    }
-
-    // If no node was provided or if the node was invalid,
-    //  generate a random MAC address and set the multicast bit
-    if (is_null($node)) {
-        $node = static::randomBytes(6);
-        $node[0] = pack("C", ord($node[0]) | 1);
-    }
-
-    $uuid .= $node;
-
-    return $uuid;
-  }
-
-  /**
-   * Insure that an input string is either binary or hexadecimal.
-   * Returns binary representation, or null on failure.
-   *
-   * @param string $str
-   * @param integer $len
-   * @return string|null
-   */
-  protected static function makeBinary($str, $len)
-  {
-    if ($str instanceof self) {
-      return $str->bytes;
-    }
-    if (strlen($str) === $len) {
-      return $str;
-    } else {
-      // strip URN scheme and namespace
-      $str = preg_replace('/^urn:uuid:/is', '', $str);
-    }
-    // strip non-hex characters
-    $str = preg_replace('/[^a-f0-9]/is', '', $str);
-
-    if (strlen($str) !== ($len * 2)) {
-      return null;
-    } else {
-      return pack("H*", $str);
-    }
-  }
-
-  protected static function timeString() {
-
-    // Get time since Gregorian calendar reform in 100ns intervals
-    $time = microtime(1) * 10000000 + static::INTERVAL;
-
-    // Convert to a string representation
-    $time = sprintf("%F", $time);
-
-    // Strip decimals
-    preg_match("/^\d+/", $time, $time);
-
-    // And now to a 64-bit binary representation
-    $time = base_convert($time[0], 10, 16);
-    $time = pack("H*", str_pad($time, 16, "0", STR_PAD_LEFT));
-
-    return $time;
-  }
-
-  /**
-   * Generates a version 3 or 5 UUID string which are derived from a name and
-   * its namespace, in binary form.
-   *
-   * @param  integer $ver  The UUID version to use. Currently versions 1,3,4 and 5 are supported
-   * @param  string  $node Name string required for version 3 or 5 UUIDs
-   * @param  string  $ns   Binary namespace is required for version 3 or 5 UUIDs
-   *
-   * @return string A fully-formatted, version 3 or 5 UUID
-   * @throws Exception
-   */
-  protected static function nameGenerator($ver, $node, $ns)
-  {
-      if (empty($node)) {
-          throw new Exception('A name-string is required for Version 3 or 5 UUIDs.');
-      }
-
-      // if the namespace UUID isn't binary, make it so
-      $ns = static::makeBinary($ns, 16);
-      if (is_null($ns)) {
-          throw new Exception('A binary namespace is required for Version 3 or 5 UUIDs.');
-      }
-
-      $version = null;
-      $uuid = null;
-
-      switch ($ver) {
-          case static::MD5:
-              $version = static::VERSION_3;
-              $uuid = md5($ns . $node, 1);
-              break;
-          case static::SHA1:
-              $version = static::VERSION_5;
-              $uuid = substr(sha1($ns . $node, 1), 0, 16);
-              break;
-          default:
-              // no default really required here
-      }
-
-      // set variant
-      $uuid[8] = chr(ord($uuid[8]) & static::CLEAR_VAR | static::VAR_RFC);
-
-      // set version
-      $uuid[6] = chr(ord($uuid[6]) & static::CLEAR_VER | $version);
-
-      return ($uuid);
-  }
 
     /**
-    * Randomness is returned as a string of bytes
+    * Generates a UUID.
     *
-    * @param $bytes
+    * @param string $node Name string required for version 3 or 5 UUIDs
     * @return string
     */
+    protected static function timeGenerator($node = null)
+    {
+
+        $time = static::timeString();
+
+        // Reorder bytes to their proper locations in the UUID
+        $uuid = $time[4] . $time[5] . $time[6] . $time[7] . $time[2] . $time[3] . $time[0] . $time[1];
+
+        // Generate a random clock sequence
+        $uuid .= random_bytes(2);
+
+        // set variant
+        $uuid[8] = chr(ord($uuid[8]) & static::CLEAR_VAR | static::VAR_RFC);
+
+        // set version
+        $uuid[6] = chr(ord($uuid[6]) & static::CLEAR_VER | static::VERSION_1);
+
+        // Set the final 'node' parameter, a MAC address
+        if (!is_null($node)) {
+            $node = static::makeBinary($node, 6);
+        }
+
+        // If no node was provided or if the node was invalid,
+        //  generate a random MAC address and set the multicast bit
+        if (is_null($node)) {
+            $node = static::randomBytes(6);
+            $node[0] = pack("C", ord($node[0]) | 1);
+        }
+
+        $uuid .= $node;
+
+        return $uuid;
+    }
+
+    /**
+    * Insure that an input string is either binary or hexadecimal.
+    * Returns binary representation, or null on failure.
+    *
+    * @param string $str
+    * @param integer $len
+    * @return string|null
+    */
+    protected static function makeBinary($str, $len)
+    {
+        if ($str instanceof self) {
+            return $str->bytes;
+        }
+        if (strlen($str) === $len) {
+            return $str;
+        } else {
+            // strip URN scheme and namespace
+            $str = preg_replace('/^urn:uuid:/is', '', $str);
+        }
+            // strip non-hex characters
+            $str = preg_replace('/[^a-f0-9]/is', '', $str);
+
+        if (strlen($str) !== ($len * 2)) {
+            return null;
+        } else {
+            return pack("H*", $str);
+        }
+    }
+
+    protected static function timeString() {
+
+        // Get time since Gregorian calendar reform in 100ns intervals
+        $time = microtime(1) * 10000000 + static::INTERVAL;
+
+        // Convert to a string representation
+        $time = sprintf("%F", $time);
+
+        // Strip decimals
+        preg_match("/^\d+/", $time, $time);
+
+        // And now to a 64-bit binary representation
+        $time = base_convert($time[0], 10, 16);
+        $time = pack("H*", str_pad($time, 16, "0", STR_PAD_LEFT));
+
+        return $time;
+    }
+
+    /**
+     * Generates a version 3 or 5 UUID string which are derived from a name and
+     * its namespace, in binary form.
+     *
+     * @param  integer $ver  The UUID version to use. Currently versions 1,3,4 and 5 are supported
+     * @param  string  $node Name string required for version 3 or 5 UUIDs
+     * @param  string  $ns   Binary namespace is required for version 3 or 5 UUIDs
+     *
+     * @return string A fully-formatted, version 3 or 5 UUID
+     * @throws Exception
+     */
+    protected static function nameGenerator($ver, $node, $ns)
+    {
+        if (empty($node)) {
+            throw new Exception('A name-string is required for Version 3 or 5 UUIDs.');
+        }
+
+        // if the namespace UUID isn't binary, make it so
+        $ns = static::makeBinary($ns, 16);
+        if (is_null($ns)) {
+            throw new Exception('A binary namespace is required for Version 3 or 5 UUIDs.');
+        }
+
+        $version = null;
+        $uuid = null;
+
+        switch ($ver) {
+            case static::MD5:
+                $version = static::VERSION_3;
+                $uuid = md5($ns . $node, 1);
+                break;
+            case static::SHA1:
+                $version = static::VERSION_5;
+                $uuid = substr(sha1($ns . $node, 1), 0, 16);
+                break;
+            default:
+                $version = static::VERSION_3;
+                $uuid = md5($ns . $node, 1);
+                break;
+        }
+
+        // set variant
+        $uuid[8] = chr(ord($uuid[8]) & static::CLEAR_VAR | static::VAR_RFC);
+
+        // set version
+        $uuid[6] = chr(ord($uuid[6]) & static::CLEAR_VER | $version);
+
+        return ($uuid);
+    }
+
+    /**
+     * Randomness is returned as a string of bytes
+     *
+     * @param $bytes
+     * @return string
+     */
     public static function randomBytes($bytes)
     {
         return random_bytes($bytes);
     }
 
     /**
-    * Generates a version 4 UUID, which are derived solely from random numbers.
-    *
-    * @return string
-    */
+     * Generates a version 4 UUID, which are derived solely from random numbers.
+     *
+     * @return string
+     */
     protected static function randomGenerator()
     {
         $uuid = static::randomBytes(16);
@@ -288,32 +290,32 @@ class Uuid
     }
 
     /**
-    * Import a pre-existing UUID
-    *
-    * @param string $uuid
-    * @return Uuid
-    */
+     * Import a pre-existing UUID
+     *
+     * @param string $uuid
+     * @return Uuid
+     */
     public static function import($uuid)
     {
         return new static(static::makeBinary($uuid, 16));
     }
 
     /**
-    * Import and validate an UUID
-    *
-    * @param Uuid|string $uuid
-    *
-    * @return boolean
-    */
+     * Import and validate an UUID
+     *
+     * @param Uuid|string $uuid
+     *
+     * @return boolean
+     */
     public static function validate($uuid)
     {
         return (boolean) preg_match('~' . static::REGEX . '~', static::import($uuid)->string);
     }
 
     /**
-    * @param string $var
-    * @return string|string|number|number|number|number|number|NULL|number|NULL|NULL
-    */
+     * @param string $var
+     * @return string|string|number|number|number|number|number|NULL|number|NULL|NULL
+     */
     public function __get($var)
     {
         switch ($var) {
@@ -372,13 +374,12 @@ class Uuid
     }
 
     /**
-    * Return the UUID
-    *
-    * @return string
-    */
+     * Return the UUID
+     *
+     * @return string
+     */
     public function __toString()
     {
         return $this->string;
     }
-
 }
