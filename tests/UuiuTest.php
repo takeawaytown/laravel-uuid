a58fa1507112f3cf6c35871ddafd7dc4c602a2ba
Alex Scott <codetheorist@gmail.com>
2017-11-14 16:46:32 +0000

    Flesh out class with generators and add simple tests for each version

--- tests/UuiuTest.php
+++ tests/UuiuTest.php
@@ -10,16 +10,19 @@ class UuidTest extends TestCase
     $uuid = Uuid::generate(1);
     $this->assertInstanceOf('TakeawayTown\LaravelUuid\Uuid', $uuid);
   }
+
   public function testVersionThreeGeneration()
   {
     $uuid = Uuid::generate(3, 'takeawaytown.co.uk', Uuid::NS_DNS);
     $this->assertInstanceOf('TakeawayTown\LaravelUuid\Uuid', $uuid);
   }
+
   public function testVersionFourGeneration()
   {
     $uuid = Uuid::generate(4);
     $this->assertInstanceOf('TakeawayTown\LaravelUuid\Uuid', $uuid);
   }
+
   public function testVersionFiveGeneration()
   {
     $uuid = Uuid::generate(5, 'takeawaytown.co.uk', Uuid::NS_DNS);
