14b48a36816b26f84b9fb2c5da5de253faf3153d
Alex Scott <codetheorist@gmail.com>
2017-11-14 14:41:32 +0000

    Added basic Uuid service provider

---
+++ src/UuidServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TakeawayTown\Uuid;
+
+use Illuminate\Support\ServiceProvider;
+
+class UuidServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
